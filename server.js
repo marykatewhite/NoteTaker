3a0a1a4e56132a6d937040628b3774179c7389fa
MaryEtak <marykatewhite@gmail.com>
2020-03-24 20:09:30 -0400

    get, post, delete and listen

--- server.js
+++ server.js
@@ -11,33 +11,39 @@ app.use(express.static("public"));
 
 const notes = require("./db/db.json");
 
-app.get("/", function(req,res) {
+// GET
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-// GET
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-app.get("/api/notes" , function(req, res) {
+
+app.get("/api/notes", function(req, res) {
   return res.json(notes);
 });
 
-// POST 
-app.post("/api/notes", function(req,res) {
-  const newNote = {};
-  // title
-  // text
-  // id
-
+// POST
+app.post("/api/notes", function(req, res) {
+  let newNote = req.body;
+  notes.push(newNote);
+  notes.forEach((element, i) => {
+    element.id = i + 1;
+  });
+  fs.writeFileSync("db.json", JSON.stringify(notes));
 });
 
 // DELETE
 app.delete("/api/notes/:id", function(req, res) {
-  
+  const deletedID = req.params.id;
+  const deletedNote = notesInfo.findIndex((deletedID));
+  notesInfo.splice(deletedNote, 1);
+  let newDB = JSON.stringify(notes);
+  fs.writeFile('db.json', newDB);
 });
 
-
+// LISTENER
 app.listen(PORT, function() {
   console.log("Server is listening on PORT: " + PORT);
 });
