5c247938201504ef9d07e0296517e1cfd0ed6e42
MaryEtak <marykatewhite@gmail.com>
2020-04-09 18:00:47 -0400

    FIXES POST

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
